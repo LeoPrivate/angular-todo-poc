5b8ee06dd7316c23e488913ec7993f937be1f7af
LeoPrivate <leo.menschenfreund@epita.fr>
2020-03-04 14:58:18 +0100

    Add env file

---
+++ angular-frontend/set-env.ts
@@ -0,0 +1,40 @@
+import { writeFile } from 'fs';// Configure Angular `environment.ts` file path
+const dotenv = require('dotenv');
+
+const targetPath = './src/environments/environment.ts';// Load node modules
+const targetPathProd = './src/environments/environment.prod.ts';
+
+const colors = require('colors');
+require('dotenv').config();
+
+const envConfigFile = `export const environment = {
+    production: false,
+    apiUrl: '${process.env.URL}:${process.env.PORTBACKEND}'
+};
+`;
+
+const envConfigFileProd = `export const environment = {
+    production: true,
+    apiUrl: '${process.env.URL}:${process.env.PORTBACKEND}'
+ };
+ `;
+
+
+console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+console.log(colors.grey(envConfigFile));
+
+writeFile(targetPath, envConfigFile, function (err) {
+   if (err) {
+       throw console.error(err);
+   } else {
+       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+   }
+});
+
+writeFile(targetPathProd, envConfigFileProd, function (err) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+ });
