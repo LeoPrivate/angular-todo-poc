31097e34ac3af542795acd2e0f3094a812da3db4
LeoPrivate <leo.menschenfreund@epita.fr>
2020-03-05 15:47:30 +0100

    add package.lock + wrapper json-server

---
+++ nodejs-database/server.js
@@ -0,0 +1,19 @@
+var fs = require('fs'),
+jsonServer = require('json-server'),
+server = jsonServer.create(),
+router = jsonServer.router('db.json'),
+middlewares = jsonServer.defaults()
+
+const cors = require ('cors')
+const bodyParser = require ('body-parser')
+
+server.use(cors())
+server.use(bodyParser.json())
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(5432, function() {
+ console.log('json-server started on port ' + 3000)
+})
+
